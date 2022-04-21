0b2e1daa64fa9942d6a59ba336c4d9dc8b11bc6f
Amitminer <81667979+Amitminer888@users.noreply.github.com>
2022-04-21 22:15:05 +0530

    Update Main.php
    Added support for PM4

--- src/Vladu11/Trampoline/Main.php
+++ src/Vladu11/Trampoline/Main.php
@@ -5,9 +5,10 @@ namespace Vladu11\Trampoline;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\level\Level;
+use pocketmine\world\World;
 use pocketmine\level\Position;
-use pocketmine\level\particle\Particle;
-use pocketmine\level\particle\HappyVillagerParticle;
+use pocketmine\world\particle\Particle;
+use pocketmine\world\particle\HappyVillagerParticle;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\Player;
@@ -18,7 +19,7 @@ use pocketmine\block\Block;
 
 class Main extends PluginBase implements Listener {
 	
-	public function onEnable(){
+	public function onEnable() : void{
 		$this->getServer()->getPluginManager()->registerEvents($this ,$this);
 		$this->saveDefaultConfig();
 		$this->config = $this->getConfig();
@@ -27,27 +28,28 @@ class Main extends PluginBase implements Listener {
 	
 	public function onMove(PlayerMoveEvent $event){
 		$player = $event->getPlayer();
-		$x = $player->getX();
-		$y = $player->getY();
-		$z = $player->getZ();
-		$level = $player->getLevel();
-		$block = $level->getBlock($player->getSide(0));
+		$x = $player->getPosition()->getX();
+		$y = $player->getPosition()->getY();
+		$z = $player->getPosition()->getZ();
+		$level = $player->getWorld();
+		$pos = $player->getPosition();
+$block = $pos->getWorld()->getBlock($pos->down());
 		if($block->getID() == $this->config->get('Block')){
 			$direction = $player->getDirectionVector();
 			$dx = $direction->getX();
 			$dz = $direction->getZ();
 			$dy = $direction->getY();
 			if($this->config->get("Particle") == "true"){
-				$level->addParticle(new HappyVillagerParticle($player));
-				$level->addParticle(new HappyVillagerParticle(new Vector3($x-0.3, $y, $z)));
-				$level->addParticle(new HappyVillagerParticle(new Vector3($x, $y, $z-0.3)));
-				$level->addParticle(new HappyVillagerParticle(new Vector3($x+0.3, $y, $z)));
-				$level->addParticle(new HappyVillagerParticle(new Vector3($x, $y, $z+0.3)));
+				$level->addParticle($pos, new HappyVillagerParticle($player));
+				$level->addParticle($pos, new HappyVillagerParticle(new Vector3($x-0.3, $y, $z)));
+				$level->addParticle($pos, new HappyVillagerParticle(new Vector3($x, $y, $z-0.3)));
+				$level->addParticle($pos, new HappyVillagerParticle(new Vector3($x+0.3, $y, $z)));
+				$level->addParticle($pos, new HappyVillagerParticle(new Vector3($x, $y, $z+0.3)));
 			}
 			$player->setMotion(new Vector3(0, $this->config->get('Power'), 0));
 		}
 	}
-	public function onDisable(){
+	public function onDisable() : void{
 		$this->getLogger()->info(C::DARK_RED."Disabled!");
 	}
 }
